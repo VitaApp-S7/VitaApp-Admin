21c5192ccc0a07b649731c6eee19b7bf27ee5523
Dylan Nas <432592@student.fontys.nl>
2023-03-02 12:19:48 +0100

    Added deployment and moodbooster changes (#3)
    * moodbooster update
    
    * put in frontend
    
    * added return
    
    * Changes by Rense
    
    * Create azure-webapps-deploy.yml
    
    * No caching added npm install
    
    * No more warnings as errors
    
    * Edited url to match server
    
    * Removed unholy semicolon
    
    * Now only the main branch triggers the deployment
    
    ---------
    
    Co-authored-by: Rense <rensevanstiphout@gmail.com>

--- src/views/moodboosters/moodboosters.js
+++ src/views/moodboosters/moodboosters.js
@@ -1,6 +1,22 @@
 import React, { useEffect, useState } from "react"
-import { getAllActivities, createActivity, deleteActivityById } from "../../services/moodboosterService"
-import { CButton, CListGroup, CModalTitle, CListGroupItem, CModal, CModalHeader, CModalBody, CModalFooter, CFormInput, CFormLabel } from "@coreui/react"
+import {
+  createActivity,
+  deleteActivityById,
+  getAllActivities,
+  updateActivity
+} from "../../services/moodboosterService"
+import {
+  CButton,
+  CFormInput,
+  CFormLabel,
+  CListGroup,
+  CListGroupItem,
+  CModal,
+  CModalBody,
+  CModalFooter,
+  CModalHeader,
+  CModalTitle
+} from "@coreui/react"
 import { useMsal } from "@azure/msal-react"
 import { loginRequest } from "../../authConfig"
 
@@ -9,28 +25,59 @@ const Moodboosters = () => {
   const [ accessToken, setAccessToken ] = useState(null)
   const [ data, setData ] = useState([])
   const [ deleteData, setDeleteData ] = useState("")
+  const [ editData, setEditData ] = useState("")
   const [ isOpen, setIsOpen ] = useState(false)
   const [ textField1, setTextField1 ] = useState("")
   const [ textField2, setTextField2 ] = useState("")
   const [ textField3, setTextField3 ] = useState("")
   const [ textField4, setTextField4 ] = useState("")
+  const [ textEditField1, setTextEditField1 ] = useState("")
+  const [ textEditField2, setTextEditField2 ] = useState("")
+  const [ textEditField3, setTextEditField3 ] = useState("")
+  const [ textEditField4, setTextEditField4 ] = useState("")
+  const [ textEditId, setTextEditId ] = useState("")
   const [ deleteModalVisible, setDeleteModalVisible ] = useState(false)
+  const [ editModalVisible, setEditModalVisible ] = useState(false)
 
   const ListItem = (item) => {
+
     return (
       <CListGroupItem>
         <b>{item.item.title}</b>
         <p>{item.item.description}</p>
-        <div style={buttons} className="d-grid gap-2 d-md-flex justify-content-md-end">
-          <CButton color="dark" variant="outline" className="float-right" onClick={() => onEdit(item)}>Edit</CButton>
-          <CButton color="warning" className="float-right" onClick={() => onDelete(item)}>Delete</CButton>
+        <div
+          style={buttons}
+          className="d-grid gap-2 d-md-flex justify-content-md-end"
+        >
+          <CButton
+            color="dark"
+            variant="outline"
+            className="float-right"
+            onClick={() => onEdit(item)}
+          >
+            Edit
+          </CButton>
+          <CButton
+            color="warning"
+            className="float-right"
+            onClick={() => onDelete(item)}
+          >
+            Delete
+          </CButton>
         </div>
       </CListGroupItem>
     )
   }
   // eslint-disable-next-line no-unused-vars
   const onEdit = (item) => {
+    setEditData(item)
 
+    setTextEditField1(item.item.title)
+    setTextEditField2(item.item.description)
+    setTextEditField3(item.item.category.name)
+    setTextEditField4(item.item.points)
+    setTextEditId(item.item.id)
+    setEditModalVisible(true)
   }
   const onDelete = (item) => {
     setDeleteData(item)
@@ -44,18 +91,26 @@ const Moodboosters = () => {
   const DeleteModal = () => {
     const item = deleteData.item
     return (
-      <CModal
-        visible={deleteModalVisible}
-        onClose={handleCancel}
-      >
+      <CModal visible={deleteModalVisible} onClose={handleCancel}>
         <CModalHeader>
-          <CModalTitle>Are you sure you want to delete this moodbooster?</CModalTitle>
+          <CModalTitle>
+            Are you sure you want to delete this moodbooster?
+          </CModalTitle>
         </CModalHeader>
         <CModalFooter>
-          <CButton color="dark" variant="outline" onClick={() => handleCancel()}>Cancel</CButton>
-          <CButton color="warning" onClick={() => deleteMoodbooster(item)}>Delete</CButton >
+          <CButton
+            color="dark"
+            variant="outline"
+            onClick={() => handleCancel()}
+          >
+            Cancel
+          </CButton>
+          <CButton color="warning" onClick={() => deleteMoodbooster(item)}>
+            Delete
+          </CButton>
         </CModalFooter>
-      </CModal>)
+      </CModal>
+    )
   }
 
   const handleSave = async () => {
@@ -68,14 +123,35 @@ const Moodboosters = () => {
     try {
       await createActivity(postData, accessToken)
       setIsOpen(false)
-      handleActivities()
+      handleCancel()
+      await handleActivities()
     } catch (error) {
       console.error("Error:", error)
     }
   }
+  const handleUpdate = async () => {
+    const postData = {
+      id: textEditId,
+      title: textEditField1,
+      description: textEditField2,
+      category: textEditField3,
+      points: textEditField4,
+      status: "ACTIVE"
+    }
+    try {
+      await updateActivity(postData, accessToken)
+      setIsOpen(false)
+      handleCancel()
+      await handleActivities()
+    } catch (error) {
+      console.error("Error:", error)
+    }
+  }
+
   const handleCancel = () => {
     setIsOpen(false)
     setDeleteModalVisible(false)
+    setEditModalVisible(false)
   }
 
   useEffect(() => {
@@ -89,23 +165,29 @@ const Moodboosters = () => {
     }
 
     // Silently acquires an access token which is then attached to a request for Microsoft Graph data
-    await instance.acquireTokenSilent(request).then((response) => {
-      setAccessToken(response.accessToken)
-    }).then(() => {
-      if (accessToken) {
-        handleActivities()
-      }
-    // eslint-disable-next-line no-unused-vars
-    }).catch((e) => {
-      instance.acquireTokenPopup(request).then((response) => {
+    await instance
+      .acquireTokenSilent(request)
+      .then((response) => {
         setAccessToken(response.accessToken)
-      }).then(() => {
+      })
+      .then(() => {
         if (accessToken) {
           handleActivities()
         }
+        // eslint-disable-next-line no-unused-vars
       })
-    })
-
+      .catch(() => {
+        instance
+          .acquireTokenPopup(request)
+          .then((response) => {
+            setAccessToken(response.accessToken)
+          })
+          .then(() => {
+            if (accessToken) {
+              handleActivities()
+            }
+          })
+      })
   }
 
   const handleActivities = async () => {
@@ -116,7 +198,9 @@ const Moodboosters = () => {
   return (
     <>
       <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-        <CButton color="dark" style={buttons} onClick={() => setIsOpen(true)}>New moodbooster</CButton>
+        <CButton color="dark" style={buttons} onClick={() => setIsOpen(true)}>
+          New moodbooster
+        </CButton>
       </div>
       <CModal visible={isOpen} onClose={handleCancel}>
         <CModalHeader closeButton>
@@ -125,20 +209,113 @@ const Moodboosters = () => {
         <CModalBody>
           <form>
             <CFormLabel htmlFor="exampleFormControlTextarea1">Title</CFormLabel>
-            <CFormInput placeholder="" value={textField1} id="exampleFormControlTextarea1" onChange={(e) => setTextField1(e.target.value)} ></CFormInput>
-            <CFormLabel htmlFor="exampleFormControlTextarea1">Description</CFormLabel>
-            <CFormInput placeholder="" value={textField2} id="exampleFormControlTextarea1" maxLength="50" onChange={(e) => setTextField2(e.target.value)} ></CFormInput>
-            <CFormLabel htmlFor="exampleFormControlTextarea1">Category</CFormLabel>
-            <CFormInput placeholder="" value={textField3} id="exampleFormControlTextarea1" onChange={(e) => setTextField3(e.target.value)} ></CFormInput>
-            <CFormLabel htmlFor="exampleFormControlTextarea1">Points</CFormLabel>
-            <CFormInput placeholder="" type="number" value={textField4} id="exampleFormControlTextarea1" min="1" max="5" onChange={(e) => setTextField4(e.target.value)} />
+            <CFormInput
+              placeholder=""
+              value={textField1}
+              id="exampleFormControlTextarea1"
+              onChange={(e) => setTextField1(e.target.value)}
+            ></CFormInput>
+            <CFormLabel htmlFor="exampleFormControlTextarea1">
+              Description
+            </CFormLabel>
+            <CFormInput
+              placeholder=""
+              value={textField2}
+              id="exampleFormControlTextarea1"
+              maxLength="50"
+              onChange={(e) => setTextField2(e.target.value)}
+            ></CFormInput>
+            <CFormLabel htmlFor="exampleFormControlTextarea1">
+              Category
+            </CFormLabel>
+            <CFormInput
+              placeholder=""
+              value={textField3}
+              id="exampleFormControlTextarea1"
+              onChange={(e) => setTextField3(e.target.value)}
+            ></CFormInput>
+            <CFormLabel htmlFor="exampleFormControlTextarea1">
+              Points
+            </CFormLabel>
+            <CFormInput
+              placeholder=""
+              type="number"
+              value={textField4}
+              id="exampleFormControlTextarea1"
+              min="1"
+              max="5"
+              onChange={(e) => setTextField4(e.target.value)}
+            />
           </form>
         </CModalBody>
         <CModalFooter>
-          <CButton color="primary" onClick={handleSave}>Save</CButton>
-          <CButton color="secondary" onClick={handleCancel}>Cancel</CButton>
+          <CButton color="primary" onClick={handleSave}>
+            Save
+          </CButton>
+          <CButton color="secondary" onClick={handleCancel}>
+            Cancel
+          </CButton>
         </CModalFooter>
       </CModal>
+      {/* Begin Edit */}
+      <CModal visible={editModalVisible} onClose={handleCancel}>
+        <CModalHeader closeButton>
+          <h5>Edit moodbooster</h5>
+        </CModalHeader>
+        <CModalBody>
+          <form>
+            <CFormLabel htmlFor="exampleFormControlTextarea1">
+              Title
+            </CFormLabel>
+            <CFormInput
+              placeholder=""
+              value={textEditField1}
+              id="exampleFormControlTextarea1"
+              onChange={(e) => setTextEditField1(e.target.value)}
+            ></CFormInput>
+            <CFormLabel htmlFor="exampleFormControlTextarea1">
+              Description
+            </CFormLabel>
+            <CFormInput
+              placeholder=""
+              value={textEditField2}
+              id="exampleFormControlTextarea1"
+              maxLength="50"
+              onChange={(e) => setTextEditField2(e.target.value)}
+            ></CFormInput>
+            <CFormLabel htmlFor="exampleFormControlTextarea1">
+              Category
+            </CFormLabel>
+            <CFormInput
+              placeholder=""
+              value={textEditField3}
+              id="exampleFormControlTextarea1"
+              onChange={(e) => setTextEditField3(e.target.value)}
+            ></CFormInput>
+            <CFormLabel htmlFor="exampleFormControlTextarea1">
+              Points
+            </CFormLabel>
+            <CFormInput
+              placeholder=""
+              type="number"
+              value={textEditField4}
+              id="exampleFormControlTextarea1"
+              min="1"
+              max="5"
+              onChange={(e) => setTextEditField4(e.target.value)}
+            />
+          </form>
+        </CModalBody>
+        <CModalFooter>
+          <CButton color="primary" onClick={handleUpdate}>
+            Save
+          </CButton>
+          <CButton color="secondary" onClick={handleCancel}>
+            Cancel
+          </CButton>
+        </CModalFooter>
+      </CModal>
+      {/* End Edit */}
       <DeleteModal />
       <CListGroup>
         {data.map((item, index) => (
