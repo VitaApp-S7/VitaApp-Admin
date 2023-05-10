78f82b8a130456151b0c36e80ddf1efba25624c6
Gijs Rongen <rongen.gijs@gmail.com>
2023-05-10 12:45:02 +0200

    debug commit

--- src/components/RichTextEditor.js
+++ src/components/RichTextEditor.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "trix/dist/trix.css";
 import Trix from "trix";
 import axios from "axios";
 import { uploadImage, getImageById } from "src/services/imageService";
@@ -9,46 +10,76 @@ class RichTextEditor extends Component {
     this.trixInput = React.createRef();
   }
 
+  // componentDidMount() {
+  //   this.trixInput.current.addEventListener("trix-change", (event) => {
+  //     console.log("trix-change");
+  //     this.props.onChange(event.target.innerHTML); //calling custom event
+  //   });
+
+  //   // Add event listener for image uploads
+  //   this.trixInput.current.addEventListener("trix-attachment-add", (event) => {
+  //     if (event.attachment.file) {
+  //       console.log("trix-attachment");
+  //       this.uploadFileAttachment(event.attachment);
+  //     }
+  //   });
+  // }
+
   componentDidMount() {
-    const { token } = this.props;
-    this.trixInput.current.addEventListener("trix-change", (event) => {
+    this.trixInput.current.addEventListener("trix-change", event => {
       this.props.onChange(event.target.innerHTML); //calling custom event
     });
-    this.trixInput.current.addEventListener(
-      "trix-attachment-add",
-      async (event) => {
-        const attachment = event.attachment;
-        if (attachment.file) {
-          const formData = new FormData();
-          formData.append("image", attachment.file);
-          const response = await uploadImage(formData, token);
-          console.log("id", response.id);
-          const imageUrl = await getImageById(response.id, token);
-          console.log("imageUrl", imageUrl);
-          console.log("response", response);
 
-          // Get the HTML element for the attachment
-          const attachmentHtml = attachment.toString();
-          const imgMatch = attachmentHtml.match(/<img src="(.*?)"/);
-          const imgSrc = imgMatch ? imgMatch[1] : null;
-
-          // Set the img src attribute to the image URL from the image service
-          if (imgSrc) {
-            const img = new Image();
-            img.onload = function () {
-              attachment.setAttributes({
-                url: imageUrl,
-                href: imageUrl,
-                width: img.width,
-                height: img.height,
-              });
-            };
-            img.src = imgSrc.replace("blob:", "");
-          }
-        }
-      }
-    );
+    // Add event listener for trix-attachment-add
+    this.trixInput.current.addEventListener("trix-attachment-add", this.handleAttachmentAdd);
   }
+
+  handleAttachmentAdd = (event) => {
+    const attachment = event.attachment;
+    if (attachment.file) {
+      this.uploadFileAttachment(attachment);
+    }
+  };
+
+  async uploadFileAttachment(attachment) {
+    const file = attachment.file;
+    const formData = new FormData();
+
+    formData.append("Content-Type", file.type);
+    formData.append("image", file);
+
+
+    let responseData = await uploadImage(formData, this.props.token);
+    console.log(responseData);
+    // const url = responseData.url;
+    // const attributes = {
+    //   url: url,
+    //   href: url + "?content-disposition=attachment",
+    // };
+
+    const url = responseData.url;
+    console.log("url", url);
+    //const id = responseData.id;
+    //let url = "http:localhost:5010/image/" + id;
+    attachment.setAttributes({
+      url: url,
+      href: `\${url}?content-disposition=attachment`,
+      src: url, // Set the correct src attribute
+    });
+
+    // Force Trix to re-render the attachment with the updated attributes
+    const editor = this.trixInput.current.editor;
+    editor.updateContents((delta) => delta.retain(editor.getDocument().length));
+
+    // Replace the image src in the editor
+    const imgElement = editor.element.querySelector(`img[data-trix-store-key^="imageElement/\${attachment.attachmentId}"]`);
+    if (imgElement) {
+      imgElement.setAttribute("src", url);
+      imgElement.setAttribute("data-trix-store-key", `imageElement/\${attachment.attachmentId}/\${url}`);
+    }
+
+  }
+
 
   render() {
     return (
